541f219145a1394a693f47f01538d3013c6d4e36
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-03-02 22:08:48 +0530

    syntax learing

---
+++ java_script/syntax.js
@@ -0,0 +1,15 @@
+let a = 5
+let b = 5
+let z = a + b
+console.log(z)
+
+// The JavaScript syntax defines two types of values:
+// Literals (Fixed values)
+// Variables (Variable values)
+
+// An identifier is the name you give to a variable.
+// Rules for identifiers:
+// Must start with a letter, _, or $
+// Can contain digits after the first character
+// Cannot be a reserved keyword (let, const, if, etc.)
+// Are case-sensitive
--- java_script/tuto.js
+++ java_script/tuto.js
@@ -6,3 +6,9 @@ function myfunction2(){
     document.getElementById('demo2').innerHTML = "Name: Raghu"
     document.getElementById('demo2').style.color = "green"
 }
+
+console.log('hello world')
+console.log(5+4);
+alert(4+4);
+// document.window
+// window.alert('hi')
