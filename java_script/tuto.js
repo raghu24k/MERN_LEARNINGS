70b7378e32d3bf51d0602f61dbf02cb4219bafc3
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-03-02 21:35:37 +0530

    another function

--- java_script/tuto.js
+++ java_script/tuto.js
@@ -1,3 +1,8 @@
 function myfunction(){
     document.getElementById('demo').innerHTML = "paragarph changed"
 }
+
+function myfunction2(){
+    document.getElementById('demo2').innerHTML = "Name: Raghu"
+    document.getElementById('demo2').style.color = "green"
+}
