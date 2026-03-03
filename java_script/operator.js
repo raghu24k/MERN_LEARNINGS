1b493a7812f4d7f51f163915ab31ef27a8e442f5
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-03-03 10:52:29 +0530

    ope 1

---
+++ java_script/operator.js
@@ -0,0 +1,38 @@
+let a = 5
+let b = (44)/100*5
+console.log(b)
+
+let a1 = 'raghu'
+let a2 = 'kumar'
+console.log(a1 + " " + a2)
+
+a1 += ' singh'
+console.log(a1)
+
+
+// Adding Strings and Numbers
+// Adding two numbers, will return the sum as a number like 5 + 5 = 10.
+// Adding a number and a string, will return the sum as a concatenated string like 5 + "5" = "55".
+
+// Example
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello" + 5;
+
+
+let x1 = 10;
+x1 += 5;
+console.log(x,y,z,x1)
+
+
+
+let x2 = 5;
+let result = x2 > 8;
+console.log(result)
+
+let x3 = 7
+// let z1 = x3**3
+
+let z2 = Math.pow(x3,2) // this is also used for exponentional  "matho.pow(x,value(2))"
+console.log(z2)
+
