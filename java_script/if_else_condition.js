54c5d13ed3eedb90c7c1df1106ace66ac0cd82ed
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-03-04 14:51:05 +0530

    1

---
+++ java_script/if_else_condition.js
@@ -0,0 +1,27 @@
+const hour = 16
+let greet;
+if (hour < 18) {
+    greet = "good day";
+} else {
+    greet = "good evening";
+}
+
+console.log(greet)
+console.log(hour)
+
+let age  = 17
+if (age<18){
+    console.log("you are not eligble")
+}
+else{
+    console.log("you are eligible")
+}
+
+let voteage = 8
+let countery = "India"
+if (countery == 'India'){
+    if(voteage >= 18){
+        console.log('you are now eligible to vote in india')
+    }
+}
+
