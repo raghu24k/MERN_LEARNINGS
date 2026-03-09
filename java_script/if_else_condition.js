fc5ebe75c70b144f4e80d95be99e3c568d2a4ed6
rkumar430@rku.ac.in <raghuwinderkumar24k@gmail.com>
2026-03-09 22:56:46 +0530

    switch 2,3

--- java_script/if_else_condition.js
+++ java_script/if_else_condition.js
@@ -224,3 +224,82 @@ switch(n1){
     default:
         console.log('Invailid Input')
 }
+
+
+console.log('---------------------------------------------------------------------------')
+// Task 2
+
+let month = 119
+switch(month){
+    case 1:
+    console.log('January')
+    break
+
+    case 2:
+    console.log("Fed")
+    break
+
+    case 3:
+    console.log('Mar')
+    break
+
+    case 4:
+    console.log("Apr")
+    break
+
+    case 5:
+    console.log('May')
+    break
+
+    case 6:
+    console.log("June")
+    break
+
+    case 7:
+    console.log('Jul')
+    break
+
+    case 8:
+    console.log('Aug')
+    break
+
+    case 9:
+    console.log("Sep")
+    break
+
+    case 10:
+    console.log('Oct')
+    break
+
+    case 11:
+    console.log("Nov")
+    break
+
+    case 12:
+    console.log('Dec')
+    break
+
+    default:
+    console.log('Invailid Input')
+}
+
+
+console.log('---------------------------------------------------------------------------')
+ // Task 3
+
+let login = 3
+switch(login){
+    case 1:
+    console.log('Login success')
+    break
+
+    case 2:
+    console.log("Register")
+    break
+
+    case 3:
+    console.log("Exit")
+
+    default:
+    console.log("Incorrect Input")
+}
